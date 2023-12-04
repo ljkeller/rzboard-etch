3a4e86aac0d1a03a855900e200c3761affb14414
ljkeller <kellerjlucas@gmail.com>
2023-12-04 00:07:30 -0600

    Initial rust commit!
    Contemplating building exactly what already exists or simplifying
    architecture and doing minor redesign.

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+// Platform agnostic utility to flash Avnet RZBoard V2L
+
+struct FlashUtil {
+    script_dir: String
+}
+
+fn main() {
+    FlashUtil {
+        script_dir: String::from("")
+    };
+}
