c8c4ab2634543287beb93345a9587c5fd63a1b0b
ljkeller <kellerjlucas@gmail.com>
2023-12-06 00:06:25 -0600

    Manage ADB cases
    Extract ADB if unextracted. Give exe permissions if UNIX. Perform file
    checking along the way.
    
    In the future, we may be able to just use the ADB rust crate.
    
    TODO: do edge case testing of the extractor. Move extractor to new
    location.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 // Platform agnostic utility to flash Avnet RZBoard V2L
 
+use core::panic;
+
 struct FlashUtil {
     cwd: String,
 }
@@ -15,7 +17,7 @@ impl<'a> AdbExtractor<'a> {
         let operating_sys = std::env::consts::OS;
 
         let platform_tools = format!("{}/adb/platform-tools", self.cwd);
-        let already_extracted = std::path::Path::exists(std::path::Path::new(&platform_tools));
+        let already_extracted = std::path::Path::new(&platform_tools).is_dir();
 
         if already_extracted {
             println!("ADB already extracted");
@@ -37,15 +39,36 @@ impl<'a> AdbExtractor<'a> {
                 panic!("Unsupported platform: {}", operating_sys);
             }
         }
+        let archive_path = std::path::Path::new(&archive_fp);
 
-        if !std::path::Path::is_file(std::path::Path::new(&archive_fp)) {
-            panic!("Platform specific ADB archive not found: {}.", archive_fp);
+        // TODO: Check if we can use Rust's ADB client instead of maintaining the zips
+        let archive_file = match std::fs::File::open(archive_path) {
+            Ok(file) => file,
+            Err(e) => panic!("Error opening file: {:?}", e),
+        };
+
+        // let mut zip_archive =
+        // zip::read::ZipArchive::new(archive_file).unwrap();
+        let mut zip_archive = match zip::read::ZipArchive::new(&archive_file) {
+            Ok(archive) => archive,
+            Err(e) => panic!("Error creating zip_archive object: {:?}", e),
+        };
+        zip_archive
+            .extract(std::path::Path::new(&format!("{}/adb", self.cwd)))
+            .expect("Failure to extract ADB, cannot continue");
+
+        if std::env::consts::OS != "windows" {
+            // TODO: give permission to execute
+            let fastboot_fp =
+                std::fs::File::open(format!("{}/adb/platform-tools/fastboot", self.cwd))
+                    .expect("Error opening /adb/platform-tools/fastboot");
+            // Notice we can only use this on Unix systems!
+            use std::fs::Permissions;
+            use std::os::unix::fs::PermissionsExt;
+            let exe_perms = Permissions::from_mode(0o755);
+            fastboot_fp.set_permissions(exe_perms).expect("Error setting permissions on fastboot. Do you have sufficient permissions to make fastboot executable?");
         }
 
-        // TODO: Extract archive
-        // TODO: give permission to execute on non-windows systems
-
-        println!("Extracting ADB from {}", archive_fp);
     }
 }
 
