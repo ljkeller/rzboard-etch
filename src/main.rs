6905d1a1b4a2bfd9996c34673557857d0605793e
ljkeller <kellerjlucas@gmail.com>
2023-12-07 23:24:50 -0600

    Add clap
    Command line argument parsing. Current implementation is WIP.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 // Platform agnostic utility to flash Avnet RZBoard V2L
 
 extern crate pretty_env_logger;
+use clap::Parser;
+use std::path::PathBuf;
 
 mod adb_utils;
 
@@ -8,8 +10,46 @@ struct FlashUtil {
     cwd: String,
 }
 
+use log::info;
+
+// Generate platform agnostic default value for serial port path
+fn get_default_serial_port() -> PathBuf {
+    let mut path = PathBuf::from("/");
+    path.push("dev");
+    path.push("ttyUSB0");
+    path
+}
+
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+struct EtchOptions {
+    #[arg(long)]
+    bootloader: bool,
+    #[arg(long)]
+    rootfs: bool,
+    #[arg(long)]
+    full: bool,
+
+    #[arg(
+        long,
+        default_value = get_default_serial_port().into_os_string(),
+        help = "Serial port used to talk to board"
+    )]
+    serial_port: PathBuf,
+    #[arg(long, default_value_t = 115200)]
+    baud_rate: u32,
+
+    #[arg(
+        long,
+        help = "Set environment variable `RUST_LOG` to {trace|debug|info|warn|error} for logging"
+    )]
+    debug: bool,
+}
+
 fn main() {
     pretty_env_logger::init();
+    let options = EtchOptions::parse();
+
     let flash_util = FlashUtil {
         cwd: std::env::current_dir()
             .unwrap()
@@ -18,8 +58,5 @@ fn main() {
             .to_string(),
     };
 
-    let adb_extractor = adb_utils::extract::AdbExtractor {
-        cwd: &flash_util.cwd,
-    };
-    adb_extractor.extract();
+    info!("CLI options: {:?}", options);
 }
